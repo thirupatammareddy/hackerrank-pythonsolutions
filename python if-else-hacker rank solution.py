400813c8ce54d34efadf35c0fe9b69866669d3c3
thirupatammareddy <131331917+thirupatammareddy@users.noreply.github.com>
2023-05-17 12:22:02 +0530

    Add files via upload

---
+++ python if-else-hacker rank solution.py
@@ -0,0 +1,7 @@
+n=int(input())
+if n%2==1:
+    print("Weird") elif n%2==0 and 2<=n <=5:
+        print("Not Weird")
+        elif n%2==0 and 6<=n<=20:
+            print("eird")
+            else:print("Not Weird")
